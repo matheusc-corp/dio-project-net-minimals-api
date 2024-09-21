f7c8965ff30929f756dde00a53496bdc3a37925b
Matheus Carvalho <matheus.carvalho.corp@gmail.com>
2024-09-21 17:28:42 +0900

    feat: Configurada metodos e rotas para o CRUD de veiculos e validacao de login de usuario adminsitrador

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,8 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using minimal_api.Dominio.DTOs;
+using minimal_api.Dominio.Entidades;
+using minimal_api.Dominio.Interface;
+using minimal_api.Dominio.ModelViews;
+using minimal_api.Dominio.Servicos;
+using minimal_api.Infraestrutura.Db;
 
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
+
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,6 +26,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+#endregion
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,13 +41,107 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapPost("/login", (LoginDto loginDto) =>
+#region Home
+app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
+#endregion
+
+#region Administradores
+app.MapPost("/Administradores/login", ([FromBody] LoginDto loginDto, IAdministradorServico administradorServico) =>
 {
-    if (loginDto.Email == "adm@teste.com" && loginDto.Senha == "123456")
+    if (administradorServico.Login(loginDto) != null)
         return Results.Ok("Login efetuado com sucesso!");
     else
         return Results.Unauthorized();
-});
+}).WithTags("Administradores");
+#endregion
+
+#region Veiculos
+ErrosDeValidacao validaDto(VeiculoDto veiculoDto)
+{
+    var validacao = new ErrosDeValidacao()
+    {
+        Mensagens = new List<string>()
+    };
+
+    if (string.IsNullOrEmpty(veiculoDto.Nome))
+        validacao.Mensagens.Add("O nome não pode ser vazio");
+
+    if (string.IsNullOrEmpty(veiculoDto.Marca))
+        validacao.Mensagens.Add("A marca não pode ficar em branco");
+
+    if (string.IsNullOrEmpty(veiculoDto.Ano) || int.Parse(veiculoDto.Ano) < 1950)
+        validacao.Mensagens.Add("O ano não pode ser vazio ou menor do que 1950");
+
+    return validacao;
+}
+
+app.MapPost("/veiculos", ([FromBody] VeiculoDto veiculoDto, IVeiculoServico veiculoServico) =>
+{
+    var validacao = validaDto(veiculoDto);
+
+    if(validacao.Mensagens.Count > 0)
+        return Results.BadRequest(validacao);
+
+    var veiculo = new Veiculo
+    {
+        Nome = veiculoDto.Nome,
+        Marca = veiculoDto.Marca,
+        Ano = veiculoDto.Ano
+    };
+
+    veiculoServico.Incluir(veiculo);
+
+    return Results.Created($"/veiculos/{veiculo.Id}", veiculo);
+}).WithTags("Veiculos");
+
+app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico) =>
+{
+    var veiculos = veiculoServico.Todos(pagina);
+
+    return Results.Ok(veiculos);
+}).WithTags("Veiculos");
+
+app.MapGet("/veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) =>
+{
+    var veiculo = veiculoServico.BuscaPorId(id);
+
+    if (veiculo == null) return Results.NotFound();
+
+    return Results.Ok(veiculo);
+}).WithTags("Veiculos");
+
+app.MapPut("/veiculos/{id}", ([FromRoute] int id, VeiculoDto veiculoDto, IVeiculoServico veiculoServico) =>
+{
+    var validacao = validaDto(veiculoDto);
+
+    if (validacao.Mensagens.Count > 0)
+        return Results.BadRequest(validacao);
+
+    var veiculo = veiculoServico.BuscaPorId(id);
+
+    if (veiculo == null) return Results.NotFound();
+
+    veiculo.Nome = veiculoDto.Nome;
+    veiculo.Marca = veiculoDto.Marca;
+    veiculo.Ano = veiculoDto.Ano;
+
+    veiculoServico.Atualizar(veiculo);
+
+    return Results.Ok(veiculo);
+}).WithTags("Veiculos");
+
+app.MapDelete("/veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) =>
+{
+    var veiculo = veiculoServico.BuscaPorId(id);
+
+    if (veiculo == null) return Results.NotFound();
+
+    veiculoServico.Apagar(veiculo);
+
+    return Results.NoContent();
+}).WithTags("Veiculos");
+
+#endregion
 
 app.Run();
 
