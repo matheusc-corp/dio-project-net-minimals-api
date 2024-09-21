e18ed633e93f3f35831467c6a9aea0907ec13563
Matheus Carvalho <matheus.carvalho.corp@gmail.com>
2024-09-21 17:30:19 +0900

    feat: Configuracao de services para validar administrador

---
+++ minimal-api/Dominio/Servicos/AdministradorServico.cs
@@ -0,0 +1,24 @@
+﻿using minimal_api.Dominio.DTOs;
+using minimal_api.Dominio.Entidades;
+using minimal_api.Dominio.Interface;
+using minimal_api.Infraestrutura.Db;
+
+namespace minimal_api.Dominio.Servicos
+{
+    public class AdministradorServico : IAdministradorServico
+    {
+        private readonly DbContexto _contexto;
+
+        public AdministradorServico(DbContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public Administrador? Login(LoginDto loginDto)
+        {
+            var adm = _contexto.Administradores.Where(a => a.Email == loginDto.Email && a.Senha == loginDto.Senha).FirstOrDefault();
+
+            return adm;
+        }
+    }
+}
